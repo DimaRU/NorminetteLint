0e63c1879a6563d1a7fe202a0e0ec3130654ea32
Dmitriy Borovikov <dmitry@borovikov.name>
2020-12-25 16:07:52 +0300

    Commands & config

--- NorminetteLint/Norminette.swift
+++ NorminetteLint/Norminette.swift
@@ -8,21 +8,37 @@ import ArgumentParser
 
 @main
 struct Norminette: ParsableCommand {
-    @Flag(help: "Display the current version of NorminetteLint")
+    enum Command {
+        case version, rules, check
+    }
+    @Argument var files: [String] = []
+    
+    @Flag(name: .shortAndLong, help: "Display the current version of NorminetteLint")
     var version = false
     
-    @Option(name: .shortAndLong, help: "The path to the file or directory to lint")
-    var path: String = FileManager.default.currentDirectoryPath
+    @Flag(name: .long, help: "Display rules list")
+    var rulesList = false
     
-    @Flag(name: .long, help: "Downgrades errors to warnings")
-    var lenient = false
-    
+    @Flag(name: .shortAndLong, help: "Downgrades errors to warnings")
+    var warnings = false
+
     @Option(name: .shortAndLong, help: "The path to the NorminetteLint configuration file")
     var config: String = FileManager.default.currentDirectoryPath + "/.norminette.yml"
     
     mutating func run() throws {
-        let path = FileManager.default.currentDirectoryPath
-        print(path)
+        if files.isEmpty {
+            let path = FileManager.default.currentDirectoryPath
+            files.append(path)
+        }
+        let command: Command
+        if version {
+            command = .version
+        } else if rulesList {
+            command = .version
+        } else {
+            command = .check
+        }
+        print(command, self)
     }
 }
 
